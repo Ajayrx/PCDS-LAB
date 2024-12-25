65fa66fd2207eb7bd1dc46372403bd58af576c43
Ajayrx <ajay0i0know@gmail.com>
2024-12-25 11:06:11 +0530

    e63

---
+++ E6-Write a program that will accept two matrices using function and multiply them using function and show the result using function.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+void readMatrix(int mat[][10], int rows, int cols)
+{
+    printf("Enter the matrix elements (%d x %d):\n", rows, cols);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf("%d", &mat[i][j]);
+        }
+    }
+}
+void multiplyMatrices(int mat1[][10], int mat2[][10], int result[][10], int rows1, int cols1, int cols2)
+{
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < cols2; j++)
+        {
+            result[i][j] = 0;
+            for (int k = 0; k < cols1; k++)
+            {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
+void displayMatrix(int mat[][10], int rows, int cols)
+{
+    printf("Result Matrix (%d x %d):\n", rows, cols);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int mat1[10][10], mat2[10][10], result[10][10];
+    int rows1, cols1, rows2, cols2;
+    printf("Enter the number of rows and columns for the first matrix: ");
+    scanf("%d %d", &rows1, &cols1);
+
+    printf("Enter the number of rows and columns for the second matrix: ");
+    scanf("%d %d", &rows2, &cols2);
+
+    printf("Matrix multiplication is not possible. The number of columns in the first matrix must be equal to the number of rows in the second matrix.\n");
+    return 1;
+
+    if (cols1 != rows2)
+    {
+
+        readMatrix(mat1, rows1, cols1);
+        readMatrix(mat2, rows2, cols2);
+        multiplyMatrices(mat1, mat2, result, rows1, cols1, cols2); // display the result
+        displayMatrix(result, rows1, cols2);
+        return 0;
+    }
